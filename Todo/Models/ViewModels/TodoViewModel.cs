80022cd4242ad60297bedb869f5c417bb3520b01
Keegan Burnham <kjb11755@uga.edu>
2024-07-23 15:22:17 -0400

    Finished Project

---
+++ Todo/Models/ViewModels/TodoViewModel.cs
@@ -0,0 +1,14 @@
+using System.Collections.Generic;
+
+namespace Todo.Models.ViewModels
+{
+
+    public class TodoViewModel
+    {
+
+        public List<TodoItem> TodoList { get; set; }
+
+        public TodoItem Todo { get; set; }
+        
+    }
+}
